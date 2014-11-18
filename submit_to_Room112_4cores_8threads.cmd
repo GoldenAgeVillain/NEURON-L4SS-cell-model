6761455f740492dead182c18858acd0b9a296350
GoldenAgeVillain <mglur5@gmail.com>
2014-11-18 20:47:37 +0000

    Final files for dataset#10

--- submit_to_Room112_4cores_8threads.cmd
+++ submit_to_Room112_4cores_8threads.cmd
@@ -1,4 +1,8 @@
 start /B nrniv -c "perm=7" -c "scen=1" main.hoc
 start /B nrniv -c "perm=8" -c "scen=1" main.hoc
+start /B nrniv -c "perm=12" -c "scen=1" main.hoc
+start /B nrniv -c "perm=13" -c "scen=1" main.hoc
 start /B nrniv -c "perm=7" -c "scen=2" main.hoc
 start /B nrniv -c "perm=8" -c "scen=2" main.hoc
+start /B nrniv -c "perm=12" -c "scen=2" main.hoc
+start /B nrniv -c "perm=13" -c "scen=2" main.hoc
