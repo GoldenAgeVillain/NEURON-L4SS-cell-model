e1e83a2902bbfd3f55b8ca5ab293f8af6cd08d14
GoldenAgeVillain <mglur5@gmail.com>
2014-11-18 18:57:14 +0000

    Minor change to one batch script

--- submit_to_LSM710main_6cores_12threads.cmd
+++ submit_to_LSM710main_6cores_12threads.cmd
@@ -1,6 +1,12 @@
 start /B nrniv -c "perm=0" -c "scen=1" main.hoc
 start /B nrniv -c "perm=1" -c "scen=1" main.hoc
 start /B nrniv -c "perm=2" -c "scen=1" main.hoc
+start /B nrniv -c "perm=9" -c "scen=1" main.hoc
+start /B nrniv -c "perm=10" -c "scen=1" main.hoc
+start /B nrniv -c "perm=11" -c "scen=1" main.hoc
 start /B nrniv -c "perm=0" -c "scen=2" main.hoc
 start /B nrniv -c "perm=1" -c "scen=2" main.hoc
 start /B nrniv -c "perm=2" -c "scen=2" main.hoc
+start /B nrniv -c "perm=9" -c "scen=2" main.hoc
+start /B nrniv -c "perm=10" -c "scen=2" main.hoc
+start /B nrniv -c "perm=11" -c "scen=2" main.hoc
