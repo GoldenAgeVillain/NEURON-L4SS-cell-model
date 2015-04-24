f434ddbe00727a82c68bf1ef625f753b8f93584a
GoldenAgeVillain <mglur5@gmail.com>
2015-04-24 16:19:46 +0100

    Dataset#16 position#02 Fergus

--- submit_to_Room112_4cores_8threads.cmd
+++ submit_to_Room112_4cores_8threads.cmd
@@ -1,8 +1,4 @@
-start /B nrniv -c "perm=7" -c "scen=1" main.hoc
-start /B nrniv -c "perm=8" -c "scen=1" main.hoc
-start /B nrniv -c "perm=12" -c "scen=1" main.hoc
-start /B nrniv -c "perm=13" -c "scen=1" main.hoc
-start /B nrniv -c "perm=7" -c "scen=2" main.hoc
-start /B nrniv -c "perm=8" -c "scen=2" main.hoc
-start /B nrniv -c "perm=12" -c "scen=2" main.hoc
-start /B nrniv -c "perm=13" -c "scen=2" main.hoc
+start /B nrniv -c "perm=0" -c "scen=1" main.hoc
+start /B nrniv -c "perm=1" -c "scen=1" main.hoc
+start /B nrniv -c "perm=2" -c "scen=1" main.hoc
+start /B nrniv -c "perm=3" -c "scen=1" main.hoc
